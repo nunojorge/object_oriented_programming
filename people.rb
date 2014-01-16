d85360f795ba454cb143a5a374b3d202ae72eb81
nunojorge <nujo@hotmail.com>
2014-01-16 15:28:37 -0500

    step 4

--- people.rb
+++ people.rb
@@ -1,14 +1,23 @@
-class Student
+class Person
+
+	attr_accessor :name
+	
+	def initialize(name)
+		@name = name
+	end
+
+end
+
+class Student < Person
 
 	def learn  
     	puts "I get it!"  
   	end
 end
 
-class Instructor
+class Instructor < Person
 
 	def teach
 		puts "Everything in Ruby is an Object"
 	end
-
 end
